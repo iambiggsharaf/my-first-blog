39ed37bc85def4702e6172c7da864e51e4ed938b
littbird <littbirdgroup@gmail.com>
2019-10-01 10:51:30 +0600

    Added my prjects

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('binary/', views.binary, name='binary'),
+    path('form/', views.form, name='form'),
+    path('generate/', views.generate, name='generate'),
+    path('geog/', views.geog, name='geog'),
+    path('stress/', views.stress, name='stress'),
+    path('typemaster/', views.typemaster, name='typemaster'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def stress(request):
+    return render(request, 'blog/stress.html', {})
+
+def binary(request):
+    return render(request, 'blog/binary.html', {})
+
+def form(request):
+    return render(request, 'blog/form.html', {})
+
+def generate(request):
+    return render(request, 'blog/generate.html', {})
+
+def typemaster(request):
+    return render(request, 'blog/typemaster.html', {})
+
+def geog(request):
+    return render(request, 'blog/geog.html', {})
